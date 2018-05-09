a0aed1a9694cd1543687710f58745652683e5278
Lucas David Traverso <lucas6246@gmail.com>
2018-05-09 16:05:37 -0300

    Fix redux extension integration

--- src/store.js
+++ src/store.js
@@ -14,7 +14,7 @@ const middlewares = [];
 middlewares.push(routerMiddleware(history));
 
 /* eslint-disable no-underscore-dangle */
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
 // Add the reducer to your store on the `router` key
