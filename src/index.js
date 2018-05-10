30c54e68f23d1edef36f22d084fd53bef79c959e
Lucas David Traverso <lucas6246@gmail.com>
2018-05-10 15:18:52 -0300

    Added translation framework

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import { I18nextProvider } from 'react-i18next';
 
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { store, history } from './store';
+import i18n from './i18n';
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
+    <I18nextProvider i18n={i18n}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </I18nextProvider>
   </Provider>
   , document.getElementById('root'),
 );
