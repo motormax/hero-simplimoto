7f2947309f8769e93239a0b7ada4acefe3a8286b
egutter <egutter@gmail.com>
2018-05-21 16:48:56 -0300

    Should work?

--- src/components/VideoPage/VideoPage.jsx
+++ src/components/VideoPage/VideoPage.jsx
@@ -2,9 +2,10 @@
 import React, { Component } from 'react';
 import { connect } from 'twilio-video';
 import { createLocalTracks } from 'twilio-video';
+import { jwt } from 'twilio';
 
-var AccessToken = require('twilio').jwt.AccessToken;
-var VideoGrant = AccessToken.VideoGrant;
+const AccessToken = jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
 
 export default class VideoPage extends Component {
   constructor(props) {
@@ -13,9 +14,27 @@ export default class VideoPage extends Component {
     this.state = {};
   }
 
+  getToken(identity) {
+    // Create an access token which we will sign and return to the client,
+    // containing the grant we just created.
+    const token = new AccessToken(
+      process.env.REACT_APP_TWILIO_ACCOUNT_SID,
+      process.env.REACT_APP_TWILIO_API_KEY,
+      process.env.REACT_APP_TWILIO_API_SECRET
+    );
+  
+    // Assign the generated identity to the token.
+    token.identity = identity;
+  
+    // Grant the access token Twilio Video capabilities.
+    let grant = new VideoGrant();
+    token.addGrant(grant);
+    return token.toJwt();
+  }
+
   // Detach the Participant's Tracks from the DOM.
   detachParticipantTracks(participant) {
-    var tracks = Array.from(participant.tracks.values());
+    let tracks = Array.from(participant.tracks.values());
     this.detachTracks(tracks);
   }
 
@@ -32,7 +51,7 @@ export default class VideoPage extends Component {
 
   // Attach the Participant's Tracks to the DOM.
   attachParticipantTracks(participant, container) {
-    var tracks = Array.from(participant.tracks.values());
+    let tracks = Array.from(participant.tracks.values());
     this.attachTracks(tracks, container);
   }
 
@@ -43,24 +62,6 @@ export default class VideoPage extends Component {
         detachedElement.remove();
       });
     });
-  }
-
-  getToken(identity) {
-    // Create an access token which we will sign and return to the client,
-    // containing the grant we just created.
-    var token = new AccessToken(
-      process.env.REACT_APP_TWILIO_ACCOUNT_SID,
-      process.env.REACT_APP_TWILIO_API_KEY,
-      process.env.REACT_APP_TWILIO_API_SECRET
-    );
-  
-    // Assign the generated identity to the token.
-    token.identity = identity;
-  
-    // Grant the access token Twilio Video capabilities.
-    var grant = new VideoGrant();
-    token.addGrant(grant);
-    return token.toJwt();
   }
 
   handleDisconnect() {
@@ -79,7 +80,7 @@ export default class VideoPage extends Component {
   
   localTracksPromiseHandler(tracks) {
     this.previewTracks = tracks;
-    var previewContainer = this.localMedia;
+    let previewContainer = this.localMedia;
     if (!previewContainer.querySelector('video')) {
       this.attachTracks(tracks, previewContainer);
     }
@@ -95,38 +96,38 @@ export default class VideoPage extends Component {
     this.buttonLeave.style.display = 'inline';
 
     // Attach LocalParticipant's Tracks, if not already attached.
-    var previewContainer = this.localMedia;
+    let previewContainer = this.localMedia;
     if (!previewContainer.querySelector('video')) {
       this.attachParticipantTracks(room.localParticipant, previewContainer);
     }
 
     // Attach the Tracks of the Room's Participants.
-    room.participants.forEach(function(participant) {
+    room.participants.forEach((participant) => {
       this.log("Already in Room: '" + participant.identity + "'");
-      var previewContainer = this.remoteMedia;
+      let previewContainer = this.remoteMedia;
       this.attachParticipantTracks(participant, previewContainer);
     });
 
     // When a Participant joins the Room, log the event.
-    room.on('participantConnected', function(participant) {
+    room.on('participantConnected', (participant) => {
       this.log("Joining: '" + participant.identity + "'");
     });
 
     // When a Participant adds a Track, attach it to the DOM.
-    room.on('trackAdded', function(track, participant) {
-      this.log(participant.identity + " added track: " + track.kind);
-      var previewContainer = this.remoteMedia;
+    room.on('trackAdded', (track, participant) => {
+      // this.log(participant.identity + " added track: " + track.kind);
+      let previewContainer = this.remoteMedia;
       this.attachTracks([track], previewContainer);
     });
 
     // When a Participant removes a Track, detach it from the DOM.
-    room.on('trackRemoved', function(track, participant) {
+    room.on('trackRemoved', (track, participant) => {
       this.log(participant.identity + " removed track: " + track.kind);
       this.detachTracks([track]);
     });
 
     // When a Participant leaves the Room, detach its Tracks.
-    room.on('participantDisconnected', function(participant) {
+    room.on('participantDisconnected', (participant) => {
       this.log("Participant '" + participant.identity + "' left the room");
       this.detachParticipantTracks(participant);
     });
@@ -138,12 +139,12 @@ export default class VideoPage extends Component {
   }
 
   handlePreviewClick() {
-    var localTracksPromise = this.previewTracks
+    let localTracksPromise = this.previewTracks
       ? Promise.resolve(this.previewTracks)
       : createLocalTracks();
 
     this.localTracksPromiseHandler = this.localTracksPromiseHandler.bind(this);
-    localTracksPromise.then(this.localTracksPromiseHandler, function(error) {
+    localTracksPromise.then(this.localTracksPromiseHandler, (error) => {
       console.error('Unable to access local media', error);
       alert('Unable to access Camera and Microphone');
     });
@@ -162,7 +163,7 @@ export default class VideoPage extends Component {
 
     this.roomName = 'Meet-Hunk-Bike';
     this.log("Joining room '" + this.roomName + "'...");
-    var connectOptions = {
+    let connectOptions = {
       name: this.roomName,
       logLevel: 'debug'
     };
@@ -170,7 +171,7 @@ export default class VideoPage extends Component {
     // Join the Room with the token from the server and the
     // LocalParticipant's Tracks.
     this.roomJoined = this.roomJoined.bind(this);
-    connect(this.getToken(this.identityInput.value), connectOptions).then(this.roomJoined, function(error) {
+    connect(this.getToken(this.identityInput.value), connectOptions).then(this.roomJoined, (error) => {
       console.log('Could not connect to Twilio: ' + error.message);
       alert('Could not connect to Twilio');
     });
@@ -189,34 +190,40 @@ export default class VideoPage extends Component {
   render() {
     return (
       <div>
-        <div id="remote-media" ref={div => {
-            this.remoteMedia = div;
-            }}></div>
+        <div
+          id="remote-media"
+          ref={(div) => { this.remoteMedia = div; }}
+        >
+        </div>
         <div id="controls">
           <div id="preview">
             <p>Please join our channel to meet your bike</p>            
-            <div id="local-media" ref={div => {
-            this.localMedia = div;
-            }}></div>
+            <div
+              id="local-media"
+              ref={(div) => { this.localMedia = div; }}>
+            </div>
             <button id="button-preview" onClick={this.handlePreviewClick.bind(this)}>Preview My Camera</button>
           </div>
-          <div id="room-controls" ref={div => {
-            this.roomControl = div;
-            }}>
+          <div
+            id="room-controls"
+            ref={(div) => { this.roomControl = div; }}
+          >
             <p>Room Name: </p>
-            <input id="identity-name" type="text" placeholder="Enter a your name" ref={input => {
-              this.identityInput = input;
-            }}/>
-            <button id="button-join" onClick={this.handleJoinClick.bind(this)} ref={button => {
-              this.buttonJoin = button;
-            }}>Join Room</button>
-            <button id="button-leave" onClick={this.handleLeaveClick.bind(this)} ref={button => {
-              this.buttonLeave = button;
-            }}>Leave Room</button>
+            <input id="identity-name" type="text" placeholder="Enter a your name" 
+                  ref={input => {this.identityInput = input;}}
+            />
+            <button id="button-join" onClick={this.handleJoinClick.bind(this)} 
+                   ref={button => {this.buttonJoin = button;}}>
+              Join Room
+            </button>
+            <button id="button-leave" onClick={this.handleLeaveClick.bind(this)} 
+                   ref={button => {this.buttonLeave = button;}}>
+              Leave Room
+          </button>
           </div>
-          <div id="log" ref={div => {
-            this.logDiv = div;
-            }}></div>
+          <div id="log" 
+              ref={div => {this.logDiv = div;}}>
+          </div>
         </div>
       </div>
     );
