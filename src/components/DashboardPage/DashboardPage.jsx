da00af11247c9b5c959a730dcc1b09bbed49011b
Lucas David Traverso <lucas6246@gmail.com>
2018-05-22 13:36:00 -0300

    Added sticky bill

--- src/components/DashboardPage/DashboardPage.jsx
+++ src/components/DashboardPage/DashboardPage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
-import { Grid, Card, Header } from 'semantic-ui-react';
+import { Grid, Card, Header, Sticky } from 'semantic-ui-react';
 
 import CurrentBill from './CurrentBill';
 import FundingSection from './Sections/FundingSection';
@@ -20,22 +20,38 @@ class DashboardPage extends Component {
     return (
       <React.Fragment>
         <Header size="large">Dashboard</Header>
-        <Grid>
-          <Grid.Column floated="left" style={{ textAlign: 'left' }} width={10}>
-            <Card.Group>
-              <FundingSection funding={stages.funding} />
-            </Card.Group>
-            <Card.Group>
-              <FundingSection funding={stages.funding} />
-            </Card.Group>
-            <Card.Group>
-              <FundingSection funding={stages.funding} />
-            </Card.Group>
-          </Grid.Column>
-          <Grid.Column floated="right" width={6}>
-            <CurrentBill />
-          </Grid.Column>
-        </Grid>
+        <div ref={(ref) => { this.ref = ref; }}>
+          <Grid>
+            <Grid.Column floated="left" style={{ textAlign: 'left' }} width={10}>
+              <Card.Group>
+                <FundingSection funding={stages.funding} />
+              </Card.Group>
+              <Card.Group>
+                <FundingSection funding={stages.funding} />
+              </Card.Group>
+              <Card.Group>
+                <FundingSection funding={stages.funding} />
+              </Card.Group>
+              <Card.Group>
+                <FundingSection funding={stages.funding} />
+              </Card.Group>
+              <Card.Group>
+                <FundingSection funding={stages.funding} />
+              </Card.Group>
+              <Card.Group>
+                <FundingSection funding={stages.funding} />
+              </Card.Group>
+              <Card.Group>
+                <FundingSection funding={stages.funding} />
+              </Card.Group>
+            </Grid.Column>
+            <Grid.Column floated="right" width={6}>
+              <Sticky context={this.ref}>
+                <CurrentBill />
+              </Sticky>
+            </Grid.Column>
+          </Grid>
+        </div>
       </React.Fragment>
     );
   }
