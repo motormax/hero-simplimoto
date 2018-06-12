6e83184ca32a020a32efc4d9041fc226644ca86b
Laski <nlascano@eryx.co>
2018-06-12 20:23:35 -0300

    Advancements in customization

--- src/components/CustomizationPage/CustomizationPanel.jsx
+++ src/components/CustomizationPage/CustomizationPanel.jsx
@@ -1,108 +1,116 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import { Button, Header, Menu, Tab } from 'semantic-ui-react';
+import { Header, Menu, Tab, Image, List, Grid } from 'semantic-ui-react';
 import { translate } from 'react-i18next';
-import { connect } from 'react-redux';
 
 const mirrorOptions = [
   { imageURL: 'http://www.triumphdirect.co.uk/ekmps/shops/122f9c/images/oval-style-mirror-drilled-stem-2180-p.jpg' },
+  { imageURL: 'https://static.pinboard.in/whn/thumbs/whn.021.thumb.jpg' },
   { imageURL: 'https://static.pinboard.in/whn/thumbs/whn.021.thumb.jpg' },
 ];
 const seatOptions = [
   { imageURL: 'https://3.imimg.com/data3/RO/IU/MY-15304011/motorbike-seat-500x500.png' },
   { imageURL: 'https://static.pinboard.in/whn/thumbs/whn.021.thumb.jpg' },
-];
-const colorOptions = [
-  { imageURL: 'https://www.colorcombos.com/images/colors/FF0000.png' },
   { imageURL: 'https://static.pinboard.in/whn/thumbs/whn.021.thumb.jpg' },
 ];
+const colorOptions = [
+  {
+    name: 'red',
+    imageURL: 'https://www.colorcombos.com/images/colors/FF0000.png',
+  },
+  {
+    name: 'blue',
+    imageURL: 'https://www.colorcombos.com/images/colors/0000FF.png',
+  },
+  {
+    name: 'black',
+    imageURL: 'https://www.colorcombos.com/images/colors/000000.png',
+  },
+];
 
-class CustomizationChooser extends Component {
+class CustomizableHeader extends Component {
   static propTypes = {
-    options: propTypes.arrayOf(propTypes.shape({ imageURL: propTypes.string.isRequired })).isRequired,
-    chosenOption: propTypes.number.isRequired,
+    imageURL: propTypes.string.isRequired,
     customizableName: propTypes.string.isRequired,
   };
 
   render() {
     return (
-      <React.Fragment>
+      <List>
         <Header>{this.props.customizableName}</Header>
-        <img
+        <Image
           alt={this.props.customizableName}
           style={{ width: '113px' }}
-          src={this.props.options[this.props.chosenOption].imageURL}
+          src={this.props.imageURL}
         />
-      </React.Fragment>
+      </List>
     );
   }
 }
 
-class CustomizationPanel extends Component {
-  static propTypes = {
-    changeMirror: propTypes.func.isRequired,
-    chosenMirror: propTypes.number.isRequired,
-    changeSeat: propTypes.func.isRequired,
-    chosenSeat: propTypes.number.isRequired,
-    changeColor: propTypes.func.isRequired,
-    chosenColor: propTypes.number.isRequired,
-  };
 
-  customizableMenuItemFactory(customizableName, options, chosenOption, changeMethod) {
-    return {
-      menuItem: (
-        <Menu.Item key={customizableName}><CustomizationChooser
+function customizableMenuItemFactory(customizableName, options, chosenOption, changeMethod) {
+  return {
+    menuItem: (
+      <Menu.Item key={customizableName}>
+        <CustomizableHeader
           customizableName={customizableName}
-          options={options}
-          chosenOption={chosenOption}
+          imageURL={options[chosenOption].imageURL}
         />
-        </Menu.Item>),
-      render: () => (
-        <Tab.Pane attached>
-          <Button onClick={() => changeMethod(1)}>Change</Button>
-        </Tab.Pane>),
+      </Menu.Item>),
+    render: () => (
+      <Tab.Pane attached>
+        <Grid>
+          <Image
+            src={options[0].imageURL}
+            onClick={() => changeMethod(0)}
+            style={{ width: '113px' }}
+          />
+          <Image
+            src={options[1].imageURL}
+            onClick={() => changeMethod(1)}
+            style={{ width: '113px' }}
+          />
+          <Image
+            src={options[2].imageURL}
+            onClick={() => changeMethod(2)}
+            style={{ width: '113px' }}
+          />
+        </Grid>
+      </Tab.Pane>),
+  };
+}
+
+class CustomizationPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chosenMirror: 0,
+      chosenSeat: 0,
+      chosenColor: 0,
     };
   }
 
+  changeMirror = (option) => {
+    this.setState({ chosenMirror: option });
+  };
+
+  changeSeat = (option) => {
+    this.setState({ chosenSeat: option });
+  };
+
+  changeColor = (option) => {
+    this.setState({ chosenColor: option });
+  };
+
   render() {
     const panes = [
-      this.customizableMenuItemFactory(
-        'Mirror', mirrorOptions, this.props.chosenMirror,
-        this.props.changeMirror,
-      ),
-      this.customizableMenuItemFactory(
-        'Seat', seatOptions, this.props.chosenSeat,
-        this.props.changeSeat,
-      ),
-      this.customizableMenuItemFactory(
-        'Color', colorOptions, this.props.chosenColor,
-        this.props.changeColor,
-      ),
+      customizableMenuItemFactory('Mirror', mirrorOptions, this.state.chosenMirror, this.changeMirror),
+      customizableMenuItemFactory('Seat', seatOptions, this.state.chosenSeat, this.changeSeat),
+      customizableMenuItemFactory('Color', colorOptions, this.state.chosenColor, this.changeColor),
     ];
     return <Tab menu={{ pointing: true }} panes={panes} />;
   }
 }
 
-const mapStateToProps =
-  store => ({
-    chosenMirror: store.main.stages.customization.chosenMirror,
-    chosenSeat: store.main.stages.customization.chosenSeat,
-    chosenColor: store.main.stages.customization.chosenColor,
-  });
-
-const mapDispatchToProps = dispatch => ({
-  changeMirror: optionNumber => dispatch({
-    type: 'CHANGE_MIRROR',
-    chosenOption: optionNumber,
-  }),
-  changeSeat: optionNumber => dispatch({
-    type: 'CHANGE_SEAT',
-    chosenOption: optionNumber,
-  }),
-  changeColor: optionNumber => dispatch({
-    type: 'CHANGE_COLOR',
-    chosenOption: optionNumber,
-  }),
-});
-
-export default translate('translations')(connect(mapStateToProps, mapDispatchToProps)(CustomizationPanel));
+export default translate('translations')(CustomizationPanel);
