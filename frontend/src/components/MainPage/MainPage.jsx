33b754b92a81073e4df3072b7892bd238ffd63c6
Gabriela Iztueta <ga.iztueta@gmail.com>
2018-07-04 16:35:10 -0300

    Home ands small home cards styles

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import { connect } from 'react-redux';
-import { Button, Card, Image } from 'semantic-ui-react';
+import { Button, Card, Image, Divider } from 'semantic-ui-react';
 import { push } from 'react-router-redux';
 import { changeBikeModel } from '../../actions/beginning';
+
+import brandImgUrl from './images/marca-hero.png';
+import hankImgUrl from './images/Hunk.png';
+import ignitorImgUrl from './images/Ignitor-blue.png';
+
+import glovesImgUrl from './images/Guantes.png';
+import helmetImgUrl from './images/Casco.png';
+import trunkImgUrl from './images/Baul.png';
+
 
 class MainPage extends Component {
   static propTypes = {
@@ -16,17 +25,89 @@ class MainPage extends Component {
     const { t } = this.props;
 
     return (
-      <Card>
-        <Image src="https://www.heromotocorp.com/en-in/uploads/bike/bike_color_pic/20160714051026-color-main-383.png" />
-        <Card.Content>
-          <Card.Header>Hunk</Card.Header>
-          <Card.Meta> <span className="date">$ 20,000.00</span> </Card.Meta>
-          <Card.Description>An awesome bike</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <Button size="big" primary onClick={() => { this.props.pickBike('HUNK'); }}>{t('buy')}</Button>
-        </Card.Content>
-      </Card>
+      <div className="home">
+        <h1 className="home-title">By the bike you want <br/> <span className="emphasis">100% online</span></h1>
+
+        <div className="cards-content">
+
+          <Card className='carrousel-item'>
+            <Image  className='brand-image' src={brandImgUrl} />
+            <Image  className='bike-image' src={hankImgUrl} />
+            <h3 className="bike-name">Hunk</h3>
+            <div className="bike-accessories">
+              <div className="bike-accessory">
+                <Image src={glovesImgUrl} />
+                <p>Gloves</p>
+              </div>
+              <div className="bike-accessory">
+                <Image src={helmetImgUrl} />
+                <p>Helmet</p>
+              </div>
+              <div className="bike-accessory">
+                <Image src={trunkImgUrl} />
+                <p>Trunks</p>
+              </div>
+            </div>
+            <Card.Content>
+              <Divider />
+              <p className="price">AR$<span className="price-number">10.000</span>/ month </p>
+              <Button size="big" primary onClick={() => { this.props.pickBike('HUNK'); }}>{t('Buy')}</Button>
+            </Card.Content>
+          </Card>
+
+          <Card className='carrousel-item'>
+            <Image  className='brand-image' src={brandImgUrl} />
+            <Image  className='bike-image' src={ignitorImgUrl} />
+            <h3 className="bike-name">Ignitor</h3>
+            <div className="bike-accessories">
+              <div className="bike-accessory">
+                <Image src={glovesImgUrl} />
+                <p>Gloves</p>
+              </div>
+              <div className="bike-accessory">
+                <Image src={helmetImgUrl} />
+                <p>Helmet</p>
+              </div>
+              <div className="bike-accessory">
+                <Image src={trunkImgUrl} />
+                <p>Trunks</p>
+              </div>
+            </div>
+            <Card.Content>
+              <Divider />
+              <p className="price">AR$<span className="price-number">10.000</span>/ month </p>
+              <Button size="big" primary onClick={() => { this.props.pickBike('HUNK'); }}>{t('Buy')}</Button>
+            </Card.Content>
+          </Card>
+
+          <Card className='carrousel-item'>
+            <Image  className='brand-image' src={brandImgUrl} />
+            <Image  className='bike-image' src={hankImgUrl} />
+            <h3 className="bike-name">Hunk</h3>
+            <div className="bike-accessories">
+              <div className="bike-accessory">
+                <Image src={glovesImgUrl} />
+                <p>Gloves</p>
+              </div>
+              <div className="bike-accessory">
+                <Image src={helmetImgUrl} />
+                <p>Helmet</p>
+              </div>
+              <div className="bike-accessory">
+                <Image src={trunkImgUrl} />
+                <p>Trunks</p>
+              </div>
+            </div>
+            <Card.Content>
+              <Divider />
+              <p className="price">AR$<span className="price-number">10.000</span>/ month </p>
+              <Button size="big" primary onClick={() => { this.props.pickBike('HUNK'); }}>{t('Buy')}</Button>
+            </Card.Content>
+          </Card>
+
+        </div>
+
+      </div>
     );
   }
 }
