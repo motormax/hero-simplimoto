974ec229e1bc5f9e06cecd0641422d02f1dda337
Joel Cámera <joel.camera@10pines.com>
2018-07-25 11:02:33 -0300

    Lead added to ExAdmin

--- backend/config/config.exs
+++ backend/config/config.exs
@@ -28,6 +28,7 @@ config :ex_admin,
   modules: [
     HeroDigitalWeb.ExAdmin.Dashboard,
     HeroDigital.ExAdmin.Product.Motorcycle,
+    HeroDigital.ExAdmin.Identity.Lead,
     HeroDigital.ExAdmin.UserData.Email,
     HeroDigital.ExAdmin.UserData.Phone,
     HeroDigital.ExAdmin.UserData.PersonalData,
