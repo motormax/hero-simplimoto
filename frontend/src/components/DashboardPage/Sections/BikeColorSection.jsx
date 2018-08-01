75a6ca48e7d680c752f3b3ea92e9e3b1d01136dc
Gabriela Iztueta <ga.iztueta@gmail.com>
2018-08-01 12:53:06 -0300

    style of bike color section

--- frontend/src/components/DashboardPage/Sections/BikeColorSection.jsx
+++ frontend/src/components/DashboardPage/Sections/BikeColorSection.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import { Grid, Icon, Image, Segment } from 'semantic-ui-react';
+import { Grid, Icon, Image, Segment, Radio } from 'semantic-ui-react';
 
 
 class BikeColorSection extends Component {
@@ -28,30 +28,38 @@ class BikeColorSection extends Component {
     const { availableColors } = this.props;
 
     const colorOptions = availableColors.map(color => (
-      <Grid.Column width={3}>
+      <div className="dashboard-card_items">
+        <Radio
+          value={selectedColor.name}
+          name='setColor'
+          onChange={() => { this.changeSelected(color); }}
+        />
+
         <Image
+          alt={color.name}
+          className="color-img"
           width="40px"
           height="40px"
           src={color.imageURL}
-          onClick={() => { this.changeSelected(color); }}
         />
-      </Grid.Column>
+
+        <p className="fw-bold txt-med-gray">{color.name}</p>
+      </div>
     ));
 
     return (
-      <Segment className="dashboard-card" style={{ borderLeftColor: '#21ba45' }}>
+      <Segment className="dashboard-card" style={{ borderLeftColor: 'darkgray' }}>
         <Grid>
           <Grid.Row>
             <Grid.Column width={1}>
-              <Icon size="large" color="green" name="check" />
+              <Icon size="large" name="arrow right" />
             </Grid.Column>
-            <Grid.Column width={10}>
-              <h3 className="fw-bold fs-big">Color de moto ({selectedColor.name})</h3>
+            <Grid.Column width={15}>
+              <h3 className="fw-bold fs-big">Personalización <span class="txt-green fs-small uppercase">¡gratis!</span></h3>
+              <div className="dashboard-card_items-container">
+                {colorOptions}
+              </div>
             </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column width={1} />
-            {colorOptions}
           </Grid.Row>
         </Grid>
       </Segment>
