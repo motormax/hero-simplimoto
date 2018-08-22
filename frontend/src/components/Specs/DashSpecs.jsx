05b877f4f6f1f328eded069f81e8611c2ca4c0ef
Gabriela Iztueta <ga.iztueta@gmail.com>
2018-08-22 00:14:43 -0300

    lint stuff

--- frontend/src/components/Specs/DashSpecs.jsx
+++ frontend/src/components/Specs/DashSpecs.jsx
@@ -1,93 +1,91 @@
 import React, { Component } from 'react';
-import { Tab, Card, Table, Segment, Button, Icon } from 'semantic-ui-react'
+import { Tab, Table, Segment, Button } from 'semantic-ui-react';
 import Slider from 'react-slick';
 
 const bikeImages = [
-  "http://www.heromotos.com.ar/medios/files/images/dash/dash 3 cuartos frente.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/dash/dash perfil.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/dash/dash atras.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/dash/dash frente.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/dash/Dash Tail Light.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/dash/Dash Seat.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/dash/Dash Console.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/dash/Dash Engine.jpg"
-]
+  'http://www.heromotos.com.ar/medios/files/images/dash/dash 3 cuartos frente.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/dash/dash perfil.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/dash/dash atras.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/dash/dash frente.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/dash/Dash Tail Light.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/dash/Dash Seat.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/dash/Dash Console.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/dash/Dash Engine.jpg',
+];
 
 const bikeInfo = [
-  {paneItems: [
-      {itemTitle:'Motor Tipo', description:'Refrigeracion por aire, 4-tiempos' },
-      {itemTitle:'Desplazamiento', description:'110.9 cc' },
-      {itemTitle:'Potencia Max.	', description:'6.2 kW @ 8000 RPM' },
-      {itemTitle:'Torque Max.	', description:'8.30 Nm @ 6500 RPM' },
-      {itemTitle:'Arranque', description:'Automatico / patada' },
+  {
+    paneItems: [
+      { itemTitle: 'Motor Tipo', description: 'Refrigeracion por aire, 4-tiempos' },
+      { itemTitle: 'Desplazamiento', description: '110.9 cc' },
+      { itemTitle: 'Potencia Max.	', description: '6.2 kW @ 8000 RPM' },
+      { itemTitle: 'Torque Max.	', description: '8.30 Nm @ 6500 RPM' },
+      { itemTitle: 'Arranque', description: 'Automatico / patada' },
     ],
-    paneTitle: 'Motor'
+    paneTitle: 'Motor',
   },
-  {paneItems: [
-      {itemTitle:'Delantera', description:'Telescopica Hidraulica' },
-      {itemTitle:'Trasera', description:'Swing con resorte precarga' },
+  {
+    paneItems: [
+      { itemTitle: 'Delantera', description: 'Telescopica Hidraulica' },
+      { itemTitle: 'Trasera', description: 'Swing con resorte precarga' },
     ],
-    paneTitle: 'Suspensión'
+    paneTitle: 'Suspensión',
   },
-  {paneItems: [
-      {itemTitle:'Tamaño Rin Delantero', description:'Campana (130 mm) - sin Asbestos' },
-      {itemTitle:'Tamaño Rin Trasero', description:'Campana (130 mm) - sin Asbestos' },
-      {itemTitle:'Tamaño llanta Delantera', description:'90/90 - 12 - 54 J Tubeless' },
-      {itemTitle:'Tamaño llanta Trasera', description:'90/100 - 10 - 53 J Tubeless' },
+  {
+    paneItems: [
+      { itemTitle: 'Tamaño Rin Delantero', description: 'Campana (130 mm) - sin Asbestos' },
+      { itemTitle: 'Tamaño Rin Trasero', description: 'Campana (130 mm) - sin Asbestos' },
+      { itemTitle: 'Tamaño llanta Delantera', description: '90/90 - 12 - 54 J Tubeless' },
+      { itemTitle: 'Tamaño llanta Trasera', description: '90/100 - 10 - 53 J Tubeless' },
     ],
-    paneTitle: 'Ruedas'
+    paneTitle: 'Ruedas',
   },
-  {paneItems: [
-      {itemTitle:'Batería', description:'12V - 4Ah (Libre de mantenimiento)' },
+  {
+    paneItems: [
+      { itemTitle: 'Batería', description: '12V - 4Ah (Libre de mantenimiento)' },
     ],
-    paneTitle: 'Eléctrico'
+    paneTitle: 'Eléctrico',
   },
-  {paneItems: [
-      {itemTitle:'Largo', description:'1841 mm' },
-      {itemTitle:'Ancho', description:'695 mm' },
-      {itemTitle:'Alto', description:'1190 mm' },
-      {itemTitle:'Altura del Sillín', description:'755 mm' },
-      {itemTitle:'Base de la Rueda', description:'1261 mm' },
-      {itemTitle:'Distancia del Suelo', description:'155 mm' },
-      {itemTitle:'Capacidad del Tanque de Gasolina', description:'5.5 L' },
-      {itemTitle:'Peso (seco)', description:'110 Kgs.' },
+  {
+    paneItems: [
+      { itemTitle: 'Largo', description: '1841 mm' },
+      { itemTitle: 'Ancho', description: '695 mm' },
+      { itemTitle: 'Alto', description: '1190 mm' },
+      { itemTitle: 'Altura del Sillín', description: '755 mm' },
+      { itemTitle: 'Base de la Rueda', description: '1261 mm' },
+      { itemTitle: 'Distancia del Suelo', description: '155 mm' },
+      { itemTitle: 'Capacidad del Tanque de Gasolina', description: '5.5 L' },
+      { itemTitle: 'Peso (seco)', description: '110 Kgs.' },
     ],
-    paneTitle: 'Dimensiones'
+    paneTitle: 'Dimensiones',
   },
-]
+];
 
-const panes = bikeInfo.map(pane => {
-  return {
-    menuItem: pane.paneTitle,
-    render: () =>
-      <Tab.Pane>
-        <Table basic='very'>
-          <Table.Body>
+const panes = bikeInfo.map(pane => ({
+  menuItem: pane.paneTitle,
+  render: () =>
+    (<Tab.Pane>
+      <Table basic="very">
+        <Table.Body>
           {
-            pane.paneItems.map(paneItem => {
-              return <Table.Row>
-                  <Table.Cell><span className="fw-bold">{paneItem.itemTitle}</span></Table.Cell>
-                  <Table.Cell>{paneItem.description}</Table.Cell>
-              </Table.Row>
-            })
+            pane.paneItems.map(paneItem => (<Table.Row>
+              <Table.Cell><span className="fw-bold">{paneItem.itemTitle}</span></Table.Cell>
+              <Table.Cell>{paneItem.description}</Table.Cell>
+                                            </Table.Row>))
           }
         </Table.Body>
       </Table>
-    </Tab.Pane>
-  }
-})
+     </Tab.Pane>),
+}));
 
-const sliderImages = bikeImages.map(url => {
-  return <div>
-      <img src={url} className="carrousel-default-images"/>
-    </div>
-})
+const sliderImages = bikeImages.map(url => (<div>
+  <img src={url} className="carrousel-default-images" alt="Foto de Dash" />
+                                            </div>));
 
-let fromHome = true;
+const fromHome = true;
 
 class HankSpecs extends Component {
-  render(){
-
+  render() {
     const settings = {
       dots: true,
       autoplay: true,
@@ -96,28 +94,28 @@ class HankSpecs extends Component {
       speed: 500,
     };
 
-    return(
+    return (
       <div className="page-column-card no-border">
-          {fromHome ?  <Button className="btn-sticky" size="massive" primary> Comprar una Dash </Button> : '' }
-          <h2 className="txt-center fs-massive">Dash</h2>
+        {fromHome ? <Button className="btn-sticky" size="massive" primary> Comprar una Dash </Button> : '' }
+        <h2 className="txt-center fs-massive">Dash</h2>
 
-          <p className="fs-huge txt-med-gray txt-center">Especificaciones técnicas</p>
+        <p className="fs-huge txt-med-gray txt-center">Especificaciones técnicas</p>
 
-          <Tab panes={panes} />
+        <Tab panes={panes} />
 
-          <Segment className="white-segment">
-            <Slider className="margin-bottom" {...settings}>
+        <Segment className="white-segment">
+          <Slider className="margin-bottom" {...settings}>
 
-               {sliderImages}
+            {sliderImages}
 
-            </Slider>
-          </Segment>
-          <div className="txt-center" attached="bottom">
+          </Slider>
+        </Segment>
+        <div className="txt-center" attached="bottom">
 
           {fromHome ?
-              <Button size="large" secondary> Volver </Button> : <Button size="large" primary> Volver </Button>
+            <Button size="large" secondary> Volver </Button> : <Button size="large" primary> Volver </Button>
             }
-          </div>
+        </div>
 
       </div>
     );
--- frontend/src/components/Specs/IgnitorSpecs.jsx
+++ frontend/src/components/Specs/IgnitorSpecs.jsx
@@ -1,109 +1,109 @@
 import React, { Component } from 'react';
-import { Tab, Card, Table, Segment, Button, Icon } from 'semantic-ui-react'
+import { Tab, Table, Segment, Button } from 'semantic-ui-react';
 import Slider from 'react-slick';
 
 const bikeImages = [
-  "http://www.heromotos.com.ar/medios/files/images/ignitor/ignitor-blue-34.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/ignitor/ignitor azul e cuartos atras.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/ignitor/ignitor azul frente.jpg",
-  "http://www.heromotos.com.ar/medios/files/images/ignitor/ignitor azul lateral.jpg",
-]
+  'http://www.heromotos.com.ar/medios/files/images/ignitor/ignitor-blue-34.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/ignitor/ignitor azul e cuartos atras.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/ignitor/ignitor azul frente.jpg',
+  'http://www.heromotos.com.ar/medios/files/images/ignitor/ignitor azul lateral.jpg',
+];
 
 const bikeInfo = [
-  {paneItems: [
-      {itemTitle:'Motor Tipo', description:'Refrigeracion por aire, 4 tiempos 1 cilindro' },
-      {itemTitle:'Desplazamiento', description:'124.7 cc' },
-      {itemTitle:'Potencia Max.	', description:'6.72 KW (9.1 Ps) @ 7000 rpm' },
-      {itemTitle:'Torque Max.	', description:'10.35 Nm @ 4000 rpm' },
-      {itemTitle:'Diámetro x Carrera', description:'52.4 x 57.8 mm' },
-      {itemTitle:'Carburador', description:'Carburador con control variable de ignicion' },
-      {itemTitle:'Relación de Compresión', description:'9.1 : 1' },
-      {itemTitle:'Arranque', description:'Electrico / patada' },
-      {itemTitle:'Encendido', description:'AMI - Advanced Microprocessor Sistema de Ignicion' },
+  {
+    paneItems: [
+      { itemTitle: 'Motor Tipo', description: 'Refrigeracion por aire, 4 tiempos 1 cilindro' },
+      { itemTitle: 'Desplazamiento', description: '124.7 cc' },
+      { itemTitle: 'Potencia Max.	', description: '6.72 KW (9.1 Ps) @ 7000 rpm' },
+      { itemTitle: 'Torque Max.	', description: '10.35 Nm @ 4000 rpm' },
+      { itemTitle: 'Diámetro x Carrera', description: '52.4 x 57.8 mm' },
+      { itemTitle: 'Carburador', description: 'Carburador con control variable de ignicion' },
+      { itemTitle: 'Relación de Compresión', description: '9.1 : 1' },
+      { itemTitle: 'Arranque', description: 'Electrico / patada' },
+      { itemTitle: 'Encendido', description: 'AMI - Advanced Microprocessor Sistema de Ignicion' },
     ],
-    paneTitle: 'Motor'
+    paneTitle: 'Motor',
   },
-  {paneItems: [
-      {itemTitle:'Embrague', description:'Multidisco' },
-      {itemTitle:'Caja de Cambios', description:'4 velocidades' },
-      {itemTitle:'Chasis', description:'Tubular doble cuna' },
+  {
+    paneItems: [
+      { itemTitle: 'Embrague', description: 'Multidisco' },
+      { itemTitle: 'Caja de Cambios', description: '4 velocidades' },
+      { itemTitle: 'Chasis', description: 'Tubular doble cuna' },
     ],
-    paneTitle: 'Transmisión'
+    paneTitle: 'Transmisión',
   },
-  {paneItems: [
-      {itemTitle:'Delantera', description:'Telescopica Hidraulica' },
-      {itemTitle:'Trasera', description:'Amortiguadores hidraulicos' },
+  {
+    paneItems: [
+      { itemTitle: 'Delantera', description: 'Telescopica Hidraulica' },
+      { itemTitle: 'Trasera', description: 'Amortiguadores hidraulicos' },
     ],
-    paneTitle: 'Suspensión'
+    paneTitle: 'Suspensión',
   },
-  {paneItems: [
-      {itemTitle:'Freno Delantero', description:'	240 mm - sin asbestos' },
-      {itemTitle:'Freno Trasero', description:'	130 mm - sin asbestos' },
+  {
+    paneItems: [
+      { itemTitle: 'Freno Delantero', description: '	240 mm - sin asbestos' },
+      { itemTitle: 'Freno Trasero', description: '	130 mm - sin asbestos' },
     ],
-    paneTitle: 'Frenos'
+    paneTitle: 'Frenos',
   },
-  {paneItems: [
-      {itemTitle:'Tamaño Rin Delantero', description:'18 x 1.60, en fudición' },
-      {itemTitle:'Tamaño Rin Trasero', description:'18 x 1.60, en fudición' },
-      {itemTitle:'Tamaño llanta Delantera', description:'2.75 x 18 - 42 P / 4 PR' },
-      {itemTitle:'Tamaño llanta Trasera', description:'3.00 x 18 - 52 P / 6 PR' },
+  {
+    paneItems: [
+      { itemTitle: 'Tamaño Rin Delantero', description: '18 x 1.60, en fudición' },
+      { itemTitle: 'Tamaño Rin Trasero', description: '18 x 1.60, en fudición' },
+      { itemTitle: 'Tamaño llanta Delantera', description: '2.75 x 18 - 42 P / 4 PR' },
+      { itemTitle: 'Tamaño llanta Trasera', description: '3.00 x 18 - 52 P / 6 PR' },
     ],
-    paneTitle: 'Ruedas'
+    paneTitle: 'Ruedas',
   },
-  {paneItems: [
-      {itemTitle:'Batería', description:'12 V - 3 Ah MF Battery' },
-      {itemTitle:'Lámpara/Luz delantera', description:'12 V - 35W/35W - Lampara (Multi-Reflector Type)' },
-      {itemTitle:'Lámpara/Luz Trasera o de Freno', description:'12 V - 5 / 21 W (Multi-Reflector)' },
-      {itemTitle:'Luz de Cruce', description:'12V - 10 W (Amber Bulb) x 4 nos. (Multi - Reflector- Clear Lens)' },
+  {
+    paneItems: [
+      { itemTitle: 'Batería', description: '12 V - 3 Ah MF Battery' },
+      { itemTitle: 'Lámpara/Luz delantera', description: '12 V - 35W/35W - Lampara (Multi-Reflector Type)' },
+      { itemTitle: 'Lámpara/Luz Trasera o de Freno', description: '12 V - 5 / 21 W (Multi-Reflector)' },
+      { itemTitle: 'Luz de Cruce', description: '12V - 10 W (Amber Bulb) x 4 nos. (Multi - Reflector- Clear Lens)' },
     ],
-    paneTitle: 'Eléctrico'
+    paneTitle: 'Eléctrico',
   },
-  {paneItems: [
-      {itemTitle:'Largo', description:'2005 mm' },
-      {itemTitle:'Ancho', description:'735 mm' },
-      {itemTitle:'Alto', description:'1070 mm' },
-      {itemTitle:'Base de la Rueda', description:'1265 mm' },
-      {itemTitle:'Distancia del Suelo', description:'150 mm' },
-      {itemTitle:'Capacidad del Tanque de Gasolina', description:'13.6 litros' },
-      {itemTitle:'Tanque de Reserva', description:'1 litro' },
-      {itemTitle:'Peso (seco)', description:'125 Kg (patada) / 129 Kg (electrico)' },
+  {
+    paneItems: [
+      { itemTitle: 'Largo', description: '2005 mm' },
+      { itemTitle: 'Ancho', description: '735 mm' },
+      { itemTitle: 'Alto', description: '1070 mm' },
+      { itemTitle: 'Base de la Rueda', description: '1265 mm' },
+      { itemTitle: 'Distancia del Suelo', description: '150 mm' },
+      { itemTitle: 'Capacidad del Tanque de Gasolina', description: '13.6 litros' },
+      { itemTitle: 'Tanque de Reserva', description: '1 litro' },
+      { itemTitle: 'Peso (seco)', description: '125 Kg (patada) / 129 Kg (electrico)' },
     ],
-    paneTitle: 'Dimensiones'
+    paneTitle: 'Dimensiones',
   },
-]
+];
 
-const panes = bikeInfo.map(pane => {
-  return {
-    menuItem: pane.paneTitle,
-    render: () =>
-      <Tab.Pane>
-        <Table basic='very'>
-          <Table.Body>
+const panes = bikeInfo.map(pane => ({
+  menuItem: pane.paneTitle,
+  render: () =>
+    (<Tab.Pane>
+      <Table basic="very">
+        <Table.Body>
           {
-            pane.paneItems.map(paneItem => {
-              return <Table.Row>
-                  <Table.Cell><span className="fw-bold">{paneItem.itemTitle}</span></Table.Cell>
-                  <Table.Cell>{paneItem.description}</Table.Cell>
-              </Table.Row>
-            })
+            pane.paneItems.map(paneItem => (<Table.Row>
+              <Table.Cell><span className="fw-bold">{paneItem.itemTitle}</span></Table.Cell>
+              <Table.Cell>{paneItem.description}</Table.Cell>
+                                            </Table.Row>))
           }
         </Table.Body>
       </Table>
-    </Tab.Pane>
-  }
-})
+     </Tab.Pane>),
+}));
 
-const sliderImages = bikeImages.map(url => {
-  return <div>
-      <img src={url} className="carrousel-default-images"/>
-    </div>
-})
+const sliderImages = bikeImages.map(url => (<div>
+  <img src={url} className="carrousel-default-images" alt="Foto de Ignitor" />
+                                            </div>));
 
-let fromHome = true;
+const fromHome = true;
 
 class HankSpecs extends Component {
-  render(){
-
+  render() {
     const settings = {
       dots: true,
       autoplay: true,
@@ -112,28 +112,28 @@ class HankSpecs extends Component {
       speed: 500,
     };
 
-    return(
+    return (
       <div className="page-column-card no-border">
-          {fromHome ?  <Button className="btn-sticky" size="massive" primary> Comprar una Ignitor</Button> : '' }
-          <h2 className="txt-center fs-massive">Nueva Ignitor con sistema i3S</h2>
+        {fromHome ? <Button className="btn-sticky" size="massive" primary> Comprar una Ignitor</Button> : '' }
+        <h2 className="txt-center fs-massive">Nueva Ignitor con sistema i3S</h2>
 
-          <p className="fs-huge txt-med-gray txt-center">Especificaciones técnicas</p>
+        <p className="fs-huge txt-med-gray txt-center">Especificaciones técnicas</p>
 
-          <Tab panes={panes} />
+        <Tab panes={panes} />
 
-          <Segment className="white-segment">
-            <Slider className="margin-bottom" {...settings}>
+        <Segment className="white-segment">
+          <Slider className="margin-bottom" {...settings}>
 
-               {sliderImages}
+            {sliderImages}
 
-            </Slider>
-          </Segment>
-          <div className="txt-center" attached="bottom">
+          </Slider>
+        </Segment>
+        <div className="txt-center" attached="bottom">
 
           {fromHome ?
-              <Button size="large" secondary> Volver </Button> : <Button size="large" primary> Volver </Button>
+            <Button size="large" secondary> Volver </Button> : <Button size="large" primary> Volver </Button>
             }
-          </div>
+        </div>
 
       </div>
     );
