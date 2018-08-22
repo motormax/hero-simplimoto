f0e2eb939eb636b67239cba57479f3a52b2e7385
Gabriela Iztueta <ga.iztueta@gmail.com>
2018-08-22 14:30:43 -0300

    lint

--- frontend/src/components/SuccessPage.jsx
+++ frontend/src/components/SuccessPage.jsx
@@ -4,17 +4,15 @@ import { Button, Card, Icon, Segment } from 'semantic-ui-react';
 const message = 'Tu pago ha sido procesado con éxito';
 
 class SuccessPage extends Component {
-
-
   render() {
-    return(
+    return (
       <div>
         <Card className="page-column-card page-column-card_slim">
           <Segment attached className="not-border-bottom">
             <h2 className="fs-massive fw-bold txt-center">Felicitaciones</h2>
           </Segment>
           <Segment attached className="txt-center txt-green">
-            <Icon name="check circle" size="massive"/>
+            <Icon name="check circle" size="massive" />
             <p className="margin-top-tinny fs-big txt-med-gray txt-center">{message}</p>
           </Segment>
           <Segment attached className="txt-center">
@@ -22,7 +20,7 @@ class SuccessPage extends Component {
           </Segment>
         </Card>
       </div>
-    )
+    );
   }
 }
 
