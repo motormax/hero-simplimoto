7fe4e96b9f4cef7c96338af42ff8927ded5aa4b9
Joel Cámera <joel.camera@10pines.com>
2018-08-28 11:34:29 -0300

    added plate registration type view

---
+++ backend/lib/hero_digital_web/views/plate_registration_type_view.ex
@@ -0,0 +1,20 @@
+defmodule HeroDigitalWeb.PlateRegistrationTypeView do
+  use HeroDigitalWeb, :view
+  alias HeroDigitalWeb.PlateRegistrationTypeView
+
+  def render("index.json", %{plate_registration_types: plate_registration_types}) do
+    %{data: render_many(plate_registration_types, PlateRegistrationTypeView, "plate_registration_type.json")}
+  end
+
+  def render("show.json", %{plate_registration_type: plate_registration_type}) do
+    %{data: render_one(plate_registration_type, PlateRegistrationTypeView, "plate_registration_type.json")}
+  end
+
+  def render("plate_registration_type.json", %{plate_registration_type: plate_registration_type}) do
+    %{
+      id: plate_registration_type.id,
+      name: plate_registration_type.name,
+      price: plate_registration_type.price,
+    }
+  end
+end
