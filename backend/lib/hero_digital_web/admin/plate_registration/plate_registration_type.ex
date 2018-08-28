acfba7af07faa429ab830eab6ef4d0428219d4b1
Joel Cámera <joel.camera@10pines.com>
2018-08-28 11:33:56 -0300

    Added plate registration type to ex admin

---
+++ backend/lib/hero_digital_web/admin/plate_registration/plate_registration_type.ex
@@ -0,0 +1,7 @@
+defmodule HeroDigital.ExAdmin.PlateRegistration.PlateRegistrationType do
+  use ExAdmin.Register
+
+  register_resource HeroDigital.PlateRegistration.PlateRegistrationType do
+
+  end
+end
