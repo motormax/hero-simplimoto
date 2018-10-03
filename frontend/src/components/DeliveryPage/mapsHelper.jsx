ac2249d6dbb430c9d5947f689ab2d13edd23bd20
Emilio Gutter <egutter@10pines.com>
2018-10-03 13:44:43 -0300

    change google API key

--- frontend/src/components/DeliveryPage/mapsHelper.jsx
+++ frontend/src/components/DeliveryPage/mapsHelper.jsx
@@ -1,6 +1,6 @@
 import scriptjs from 'scriptjs';
 
-const googleMapScriptUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDw0hy_2xjxbuTnwOVTTirzBdYhBd5I6aw&libraries=places';
+const googleMapScriptUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC0Q1nB6mj4Fs38R8FRjhuQY460hxJqtmQ&libraries=places';
 
 async function loadMap(callback) {
   scriptjs(googleMapScriptUrl, callback);
