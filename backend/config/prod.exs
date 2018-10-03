521b3bfe361b486f0685dac499c07ce36972523b
Emilio Gutter <egutter@10pines.com>
2018-10-03 13:19:44 -0300

    Force SSL

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -15,8 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :hero_digital, HeroDigitalWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "heromotodigital.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
