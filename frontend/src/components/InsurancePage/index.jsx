7e539c9cd5e937a4dfc965d62f69d555a5ab5284
egutter <egutter@gmail.com>
2018-10-12 15:50:45 -0300

    Adjust insurance policy img size

--- frontend/src/components/InsurancePage/index.jsx
+++ frontend/src/components/InsurancePage/index.jsx
@@ -165,7 +165,7 @@ class InsurancePage extends Component {
     return (
       <Card key={quote.policyId}>
         <Card.Content textAlign="center">
-          <Image src={quote.brokerLogo} />
+          <Image src={quote.brokerLogo} width="150px" />
           <Card.Description>{quote.policy}</Card.Description>
           <Card.Meta>{this.popUpMoreInfo(quote)}</Card.Meta>
         </Card.Content>
