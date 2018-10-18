4ff2fb35c4e7f942ca6100fb9085e5ad7e17530b
dserrot <43821669+dserrot@users.noreply.github.com>
2018-10-18 13:44:52 -0300

    Change references from Hero Moto Corp
    Change references from Hero Moto Corp to Distribuidora Argentina de Motos S.A.

--- frontend/src/components/legal/responsibilityDisclaimer.jsx
+++ frontend/src/components/legal/responsibilityDisclaimer.jsx
@@ -16,20 +16,20 @@ class ResponsibilityDisclaimer extends Component {
         <h2 className="fs-massive txt-dark-gray fw-bold">DESCARGO DE RESPONSABILIDAD</h2>
         <p className="fs-large txt-dark-gray">
           Todo el contenido pero no limitado al texto, gráficos, diseños y marcas de este sitio
-          es propiedad de © Hero MotoCorp. Puede descargar el contenido sólo para uso personal
+          es propiedad de Distribuidora Argentina de Motos S.A. Puede descargar el contenido sólo para uso personal
           y sólo sin propósitos comerciales. La modificación del contenido, reproducción o
           incorporación en cualquier trabajo, publicación o sito, ya sea en copia impresa o
           formato electrónico, como publicaciones en cualquier otro sitio, está estrictamente
-          prohibido y Hero MotoCorp tiene todos los derechos reservados.
+          prohibido y Distribuidora Argentina de Motos S.A. tiene todos los derechos reservados.
         </p>
         <p className="fs-large txt-dark-gray">
-          Hero MotoCorp hará todo lo posible para incluir la información precisa en el sitio y
-          sólo con propósitos generales. Hero MotoCorp no tiene representaciones, garantías o
+          Distribuidora Argentina de Motos S.A. hará todo lo posible para incluir la información precisa en el sitio y
+          sólo con propósitos generales. Distribuidora Argentina de Motos S.A. no tiene representaciones, garantías o
           seguridad sobre la precisión, difusión o integridad de la información provista.
         </p>
         <p className="fs-large txt-dark-gray">
-          La información sobre este sitio no constituye una incitación a invertir en Hero MotoCorp
-          y Hero MotoCorp o sus representantantes, agentes empleadores no serán responsables de
+          La información sobre este sitio no constituye una incitación a invertir en Distribuidora Argentina de Motos S.A.
+          y Distribuidora Argentina de Motos S.A. o sus representantantes, agentes empleadores no serán responsables de
           ninguna pérdida, daño o gastos ya sean directos, indirectos, imprevistos o indirectos,
           provocados debido al acceso a este sitio y cualquier sitio conectado al mismo.
         </p>
