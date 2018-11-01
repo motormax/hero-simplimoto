8a3f43387167347996745e92f2239250594a5e5d
Luciano Bernal <luciano.bernal@gmail.com>
2018-11-01 00:20:14 -0300

    Add installment message

--- backend/lib/hero_digital_web/views/credicuotas_view.ex
+++ backend/lib/hero_digital_web/views/credicuotas_view.ex
@@ -7,6 +7,12 @@ defmodule HeroDigitalWeb.CredicuotasView do
   end
 
   def render("installment.json", %{credicuotas: installment}) do
-    %{amount: installment["amount"], installments: installment["installments"]}
+    %{amount: installment["amount"], installments: installment["installments"], message: installment_message(installment)}
+  end
+
+  defp installment_message(%{"amount" => amount, "installments" => installments}) do
+    formatted_amount = Float.to_string(amount, decimals: 2)
+    formatted_total = Float.to_string(installments * amount, decimals: 2)
+    "#{installments} #{installments == 1 && "cuota" || "cuotas"} de $#{formatted_amount} (#{formatted_total})"
   end
 end
--- backend/test/hero_digital_web/controllers/credicuotas_controller_test.exs
+++ backend/test/hero_digital_web/controllers/credicuotas_controller_test.exs
@@ -6,13 +6,41 @@ defmodule HeroDigitalWeb.CredicuotasControllerTest do
   import Mox
 
   @success_body ([
-                   %{"amount" => 3087.34, "installments" => 6},
-                   %{"amount" => 2325.67, "installments" => 9},
-                   %{"amount" => 1988.92, "installments" => 12},
-                   %{"amount" => 1781.63, "installments" => 15},
-                   %{"amount" => 1652.5, "installments" => 18},
-                   %{"amount" => 1560.7, "installments" => 21},
-                   %{"amount" => 1502.2, "installments" => 24}
+                   %{
+                     "amount" => 3087.34,
+                     "installments" => 6,
+                     "message" => "6 cuotas de $3087.34 (18524.04)"
+                   },
+                   %{
+                     "amount" => 2325.67,
+                     "installments" => 9,
+                     "message" => "9 cuotas de $2325.67 (20931.03)"
+                   },
+                   %{
+                     "amount" => 1988.92,
+                     "installments" => 12,
+                     "message" => "12 cuotas de $1988.92 (23867.04)"
+                   },
+                   %{
+                     "amount" => 1781.63,
+                     "installments" => 15,
+                     "message" => "15 cuotas de $1781.63 (26724.45)"
+                   },
+                   %{
+                     "amount" => 1652.5,
+                     "installments" => 18,
+                     "message" => "18 cuotas de $1652.50 (29745.00)"
+                   },
+                   %{
+                     "amount" => 1560.7,
+                     "installments" => 21,
+                     "message" => "21 cuotas de $1560.70 (32774.70)"
+                   },
+                   %{
+                     "amount" => 1502.2,
+                     "installments" => 24,
+                     "message" => "24 cuotas de $1502.20 (36052.80)"
+                   }
                  ])
 
   test "render installments", %{conn: conn} do
