7ea8296eae502d285f001eb58aa9714ddd16f318
Luciano Bernal <luciano.bernal@gmail.com>
2019-01-22 16:14:45 -0300

    Code refactor + Basic 123Seguros form

--- frontend/src/components/InsurancePage/constants.jsx
+++ frontend/src/components/InsurancePage/constants.jsx
@@ -2,4 +2,5 @@ export const PROVINCE_CABA = 'Capital Federal';
 export const PROVINCE_BSAS = 'Gran Buenos Aires';
 export const PERSONAL_INSURANCE = 'personalInsurance';
 export const HERO_INSURANCE = 'heroInsurance';
+export const UN23_INSURANCE = '123Insurance';
 
