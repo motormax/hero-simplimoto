f298e77b08e3107303ab28fbe1bd4396a54cbfd5
Luciano Bernal <luciano.bernal@MacBook-Pro-de-Luciano.local>
2019-01-29 12:03:42 -0300

    bank transfer done

--- frontend/src/components/FinancingPage/BankTransferForm.jsx
+++ frontend/src/components/FinancingPage/BankTransferForm.jsx
@@ -13,15 +13,19 @@ class BankTransferForm extends Component {
     return (
       <Card className="page-column-card financing-page">
         <Segment attached className="txt-center">
-          <p className="txt-dark-gray fw-bold fs-big">1- Hacé un depósito o una transferencia bancaria a nuestra cuenta.</p>
-          <p className="fs-large txt-med-gray txt-center"><b>Alias: </b>HEROMOTO</p>
-          <p className="fs-large txt-med-gray txt-center">O</p>
+          <p className="txt-dark-gray fw-bold fs-big">1- Hacé un depósito o una transferencia bancaria a cualquiera de nuestras cuentas.</p>
           <p className="fs-large txt-med-gray txt-center"><b>Banco: </b>Banco Galicia</p>
           <p className="fs-large txt-med-gray txt-center"><b>Tipo de cuenta: </b>Cuenta corriente en Pesos</p>
-          <p className="fs-large txt-med-gray txt-center"><b>Número de cuenta: </b>035-005198/5</p>
-          <p className="fs-large txt-med-gray txt-center"><b>CBU: </b>0720035920000000519854</p>
-          <p className="fs-large txt-med-gray txt-center"><b>A nombre de: </b>MOVILIZAR SRL</p>
-          <p className="fs-large txt-med-gray txt-center"><b>CUIT: </b>30-71201935-9</p>
+          <p className="fs-large txt-med-gray txt-center"><b>Número de cuenta: </b>6001-3 019-1</p>
+          <p className="fs-large txt-med-gray txt-center"><b>CBU: </b>00700191-20000006001315</p>
+          <p className="fs-large txt-med-gray txt-center">O</p>
+          <p className="fs-large txt-med-gray txt-center"><b>Banco: </b>Banco Santander Río</p>
+          <p className="fs-large txt-med-gray txt-center"><b>Tipo de cuenta: </b>Cuenta corriente en Pesos</p>
+          <p className="fs-large txt-med-gray txt-center"><b>Número de cuenta: </b>039-004592/4</p>
+          <p className="fs-large txt-med-gray txt-center"><b>CBU: </b>00700191-20000006001315</p>
+          <p className="fs-large txt-med-gray txt-center">Empresa</p>
+          <p className="fs-large txt-med-gray txt-center"><b>A nombre de: </b>MARWEN S.A.</p>
+          <p className="fs-large txt-med-gray txt-center"><b>CUIT: </b>0720039720000000459240</p>
           <p className="txt-dark-gray fw-bold fs-big">2- Descargá el comprobante y envialo por email.</p>
           <p className="fs-large txt-med-gray txt-center">Email: ventas@heromotodigital.com</p>
           <p className="fs-large txt-med-gray txt-center">Asunto: comprobante</p>
