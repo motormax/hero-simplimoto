5e95495a20681d79702b5074810d6979f63a363d
Luciano Bernal <luciano.bernal@gmail.com>
2019-04-15 20:58:34 -0300

    Accessories page working

--- frontend/src/app/pages/accessories/accessories.component.ts
+++ frontend/src/app/pages/accessories/accessories.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
-import {fetchLead, LeadResponse} from '../../utils';
+import {AccessoriesResponse, Accessory, fetchAccessories, fetchLead, LeadResponse} from '../../utils';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
 
@@ -11,11 +11,27 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class AccessoriesComponent implements OnInit {
   lead$: Observable<LeadResponse>;
+  accessories$: Observable<AccessoriesResponse>;
   leadId: string;
+  selectedAccessories: Accessory[] = [];
 
   constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.lead$ = fetchLead.call(this);
+    this.accessories$ = fetchAccessories.call(this);
+  }
+
+  onAccessorySelected(accessory: Accessory) {
+    if (this.selectedAccessories.includes(accessory)) {
+      this.selectedAccessories = this.selectedAccessories.filter(a => a !== accessory);
+      this.http.delete(`/api/leads/${this.leadId}/accessory/${accessory.id}`)
+        .subscribe(() => console.log(`Accessory ${accessory.id} deleted`));
+    } else {
+      this.selectedAccessories = [...this.selectedAccessories, accessory];
+      this.http.post(`/api/leads/${this.leadId}/accessory/${accessory.id}`, {})
+        .subscribe(() => console.log(`Accessory ${accessory.id} added`));
+    }
+    console.log(this.selectedAccessories);
   }
 }
--- frontend/src/app/pages/customize/customize.component.ts
+++ frontend/src/app/pages/customize/customize.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs';
-import {fetchLead, LeadResponse, fetchAccessories, AccessoriesResponse, Accessory} from '../../utils';
+import {fetchLead, LeadResponse} from '../../utils';
 
 @Component({
   selector: 'app-customize',
@@ -11,27 +11,12 @@ import {fetchLead, LeadResponse, fetchAccessories, AccessoriesResponse, Accessor
 })
 export class CustomizeComponent implements OnInit {
   lead$: Observable<LeadResponse>;
-  accessories$: Observable<AccessoriesResponse>;
   leadId: string;
-  selectedAccessories: Accessory[] = [];
 
   constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.lead$ = fetchLead.call(this);
-    this.accessories$ = fetchAccessories.call(this);
   }
 
-  onAccessorySelected(accessory: Accessory) {
-    if (this.selectedAccessories.includes(accessory)) {
-      this.selectedAccessories = this.selectedAccessories.filter(a => a !== accessory);
-      this.http.delete(`/api/leads/${this.leadId}/accessory/${accessory.id}`)
-        .subscribe(() => console.log(`Accessory ${accessory.id} deleted`));
-    } else {
-      this.selectedAccessories = [...this.selectedAccessories, accessory];
-      this.http.post(`/api/leads/${this.leadId}/accessory/${accessory.id}`, {})
-        .subscribe(() => console.log(`Accessory ${accessory.id} added`));
-    }
-    console.log(this.selectedAccessories);
-  }
 }
