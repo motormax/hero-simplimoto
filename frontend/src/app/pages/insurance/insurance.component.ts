fcda38b29cce09fff234871886030449fd2d6df5
Luciano Bernal <luciano.bernal@gmail.com>
2019-04-16 00:29:50 -0300

    Personal insurance working

--- frontend/src/app/pages/insurance/insurance.component.ts
+++ frontend/src/app/pages/insurance/insurance.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {fetchLead, LeadResponse} from '../../utils';
 import {Observable} from 'rxjs';
 
@@ -12,11 +12,38 @@ import {Observable} from 'rxjs';
 export class InsuranceComponent implements OnInit {
   lead$: Observable<LeadResponse>;
   leadId: string;
+  motorcycleId: number;
+  selectedOption = 'with_hero';
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  set option(value) {
+    console.log(value);
+    this.selectedOption = value;
+  }
+
+  get option(): string {
+    return this.selectedOption;
+  }
+
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.lead$ = fetchLead.call(this);
+    this.lead$.subscribe(r => {
+      if (!r) { return; }
+
+      this.motorcycleId = r.data.motorcycle.id;
+    });
   }
 
+  continueWithoutInsurance() {
+    const body = {
+      insurance_choice: {
+        opt_in_or_out: 'personalInsurance',
+        motorcycle_id: this.motorcycleId
+      }
+    };
+
+    this.http.post(`/api/leads/${this.leadId}/insurance`, body)
+      .subscribe(() => this.router.navigate(['/accesorios', this.leadId]));
+  }
 }
