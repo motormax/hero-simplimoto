56eb61ba8cb2645960d2df6d07087c4ee12419bb
Luciano Bernal <luciano.bernal@gmail.com>
2019-04-16 00:37:04 -0300

    Personal plate registration working

--- frontend/src/app/pages/patente/patente.component.ts
+++ frontend/src/app/pages/patente/patente.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import {fetchLead, LeadResponse} from '../../utils';
 import {HttpClient} from '@angular/common/http';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-patente',
@@ -12,10 +12,30 @@ import {ActivatedRoute} from '@angular/router';
 export class PatenteComponent implements OnInit {
   lead$: Observable<LeadResponse>;
   leadId: string;
+  selectedOption = 'with_hero';
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  set option(value) {
+    console.log(value);
+    this.selectedOption = value;
+  }
+
+  get option() {
+    return this.selectedOption;
+  }
+
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.lead$ = fetchLead.call(this);
+  }
+
+  continueWithoutHero() {
+    const body = {
+      plate_registration_data: {
+        opt_in_or_out: 'personalPlateRegistration'
+      }
+    };
+    this.http.post(`/api/leads/${this.leadId}/plate_registration`, body)
+      .subscribe(() => this.router.navigate(['/envio', this.leadId]));
   }
 }
