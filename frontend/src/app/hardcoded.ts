fe123bec65f3319775ebba0ee2efe2f21589530a
Luciano Bernal <luciano.bernal@gmail.com>
2019-04-19 17:50:46 -0300

    Insurance section working

--- frontend/src/app/hardcoded.ts
+++ frontend/src/app/hardcoded.ts
@@ -13,3 +13,23 @@ export const IDS = {
 };
 
 export const NAMES = Object.keys(IDS).map(name => ({ [IDS[name]]: name })).reduce((a, e) => ({ ...a, ...e }), {});
+
+/* For humans:
+  HUNK 150: 9610002
+  IGNITOR 125: 9610003
+  HUNK 150 SPORTS: 9610001
+  DASH 110: 9610004
+*/
+export const INFOMOTO = {
+  1: 9610002,
+  2: 9610003,
+  3: 9610001,
+  4: 9610004
+};
+
+export const ISSUER_URLS = {
+  mapfre: 'https://www.123seguro.com/images/front/table/mapfre.png',
+  libra: 'https://4.bp.blogspot.com/-EbhruQjEDLo/WnRQeldOIaI/AAAAAAAABiE/zDmWBehmIOY7NzyL_kd25IgUHdYTqYSbQCLcBGAs/s400/libra.png',
+  atm: 'https://www.atmseguros.com.ar/newsitedev/wp-content/uploads/2018/06/logo-atm-.png',
+  orbis: 'https://www.123seguro.com/images/front/cotizar-auto/aseguradoras/orbis-seguros-de-autos.svg',
+};
