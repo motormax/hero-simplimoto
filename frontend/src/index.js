cced9130f1344860a190283592087b2f7fd0fea2
Luciano Bernal <luciano.bernal@gmail.com>
2019-04-30 00:06:36 +0200

    Remove sw

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ import { toast } from 'react-toastify';
 
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import unregisterServiceWorker from './unregisterServiceWorker';
 import createAppStoreAndHistory from './store';
 import i18n from './i18n';
 import { leadFetched, startedFetchingLead } from './actions/beginning';
@@ -56,7 +56,7 @@ const render = () => {
   );
 };
 render();
-registerServiceWorker();
+unregisterServiceWorker();
 
 if (module.hot) {
   module.hot.accept('./components/App', () => {
---
+++ frontend/src/unregisterServiceWorker.js
@@ -0,0 +1,19 @@
+export default function () {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations()
+      .then(
+        function (registrations) {
+          for (let registration of registrations) {
+            registration.unregister()
+              .then(unregistered => {
+                if (unregistered) {
+                  console.log('Service worker removed successfully');
+                } else {
+                  console.log('Failed to unregister service worker');
+                }
+              });
+          }
+        },
+      );
+  }
+}
