1e29c298a4e8706e675e44e18db1657ede52fffb
Luciano Bernal <luciano.bernal@gmail.com>
2019-05-15 00:30:26 -0300

    Add basic form validation

--- frontend/src/app/pages/checkout/checkout.component.ts
+++ frontend/src/app/pages/checkout/checkout.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {fetchLead, LeadResponse} from '../../utils';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -12,11 +13,31 @@ import {ActivatedRoute} from '@angular/router';
 export class CheckoutComponent implements OnInit {
   lead$: Observable<LeadResponse>;
   leadId: string;
+  form: FormGroup;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  constructor(private http: HttpClient, private route: ActivatedRoute, private formBuild: FormBuilder) { }
 
   ngOnInit() {
     this.lead$ = fetchLead.call(this);
+    this.form = this.formBuild.group({
+      fullName: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', Validators.required],
+      cardNumber: ['', Validators.required],
+      cvv: ['', Validators.required],
+      month: ['', Validators.required],
+      year: ['', Validators.required],
+      cardName: ['', Validators.required],
+      idType: ['', Validators.required],
+      idNumber: ['', Validators.required]
+    });
   }
 
+  submit() {
+    if (this.form.invalid) {
+      console.log('invalid');
+      return;
+    }
+    console.log('valid');
+  }
 }
